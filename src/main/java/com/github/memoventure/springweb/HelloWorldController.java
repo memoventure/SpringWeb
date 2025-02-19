1a3ebeb3432a478894900aec0faee353e27c3399
anna-koenig <anna-koenig@web.de>
2025-02-19 11:22:57 +0100

    implement hello world controller

--- src/main/java/com/github/memoventure/springweb/HelloWorldController.java
+++ src/main/java/com/github/memoventure/springweb/HelloWorldController.java
@@ -1,4 +1,16 @@
 package com.github.memoventure.springweb;
+import org.springframework.web.bind.annotation.*;
 
+@RestController // Indicates that the class is a controller capable of handling
+// HTTP requests and combines the @Controller and @ResponseBody annotations
+// to facilitate returning data as HTTP responses.
+@RequestMapping("/api") // Assigns a URL path to a method or controller -
+// in this case, /api is considered as the prefix for the class endpoints.
 public class HelloWorldController {
+    @GetMapping("/hello")
+    public String sayHello()
+    {
+        return "Hello my beloved World";
+    }
+
 }
